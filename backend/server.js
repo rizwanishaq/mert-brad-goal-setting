8f15264f7941e03f99758086c2235e50759e2c8b
rizwanishaq <rizwanishaq@gmail.com>
2022-02-11 12:18:41 +0100

    first part of goals CRUD completed

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+// Database connection
+connectDB();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Middle ware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
+
+// Routes
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
