ae8e2d3c5951d4a4c5ea1714a39ca8aaaafbd7ee
rizwanishaq <rizwanishaq@gmail.com>
2022-02-17 17:01:29 +0100

    modified

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -18,6 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(errorHandler);
 app.use(cors());
 
+// Server frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+}
 // Routes
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
