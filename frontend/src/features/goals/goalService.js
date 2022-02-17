89cd663172529b9352fff5cc3be49bb99b99619a
rizwanishaq <rizwanishaq@gmail.com>
2022-02-17 17:06:59 +0100

    final version

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/goals/";
+const API_URL = "/api/goals/";
 
 // Create new goal
 const createGoal = async (goalData, token) => {
